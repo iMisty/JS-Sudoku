7d99e40af3f69b7870c43b8033d557531b9f925d
JingYi <qianwumeng_2cyndi@qq.com>
2017-11-19 23:35:06 +0800

    Fisher-yates

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,7 @@
-function makeRow (v = 0) {
-  const array = new Array(9)
-  array.fill(v)
-  return array
-}
+const toolkit = require("./toolkit");
+const matrix = toolkit.makeMatrix();
+console.log(matrix);
 
-function makeMatrix (v = 0) {
-  return Array.from({length: 9}, () => makeRow(v));
-}
-/* 
-const a = makeMatrix();
-a[0][1] = 2;
+const a = Array.from({length: 9},(v, i) => i);
 console.log(a);
-*/
+console.log(toolkit.shuffle(a));
