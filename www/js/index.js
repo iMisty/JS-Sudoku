817184de20568d90ad457c81e3f1d27e4d6f6467
JingYi <qianwumeng_2cyndi@qq.com>
2017-11-23 19:51:12 +0800

    Upload Style

--- www/js/index.js
+++ www/js/index.js
@@ -44,13 +44,40 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	const toolkit = __webpack_require__(1);
-	const matrix = toolkit.makeMatrix();
-	console.log(matrix);
+	const toolkit = __webpack_require__(1)
+	const matrix = toolkit.makeMatrix()
+	console.log(matrix)
 	
-	const a = Array.from({length: 9},(v, i) => i);
-	console.log(a);
-	console.log(toolkit.shuffle(a));
+	class Grid {
+	  constructor (container) {
+	    this._$container = container
+	  }
+	  build () {
+	    const matrix = toolkit.makeMatrix();
+	
+	    const rowGroupClasses = ["row_g_top","row_g_middle","row_g_bottom"];
+	    const colGroupClasses = ["col_g_top","col_g_center","col_g_right"];
+	
+	    const $cells = matrix.map(rowValues => rowValues
+	        .map((cellValue, colIndex) => {
+	            return $("<span>")
+	                .addClass(colGroupClasses[colIndex % 3])
+	                .text(cellValue);
+	        })
+	    );
+	
+	    const $divArray = $cells.map(($spanArray, rowIndex) => {
+	        return $("<div>")
+	            .addClass("row")
+	            .addClass(rowGroupClasses[rowIndex % 3])
+	            .append($spanArray);
+	    });
+	
+	    this._$container.append($divArray);
+	  }
+	}
+	
+	new Grid($("#container")).build();
 
 /***/ }),
 /* 1 */
