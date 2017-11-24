0be50309c829019621a19f56fb99949c69282e81
JingYi <qianwumeng_2cyndi@qq.com>
2017-11-24 19:17:25 +0800

    检查数组解决方案

--- src/js/core/checker.js
+++ src/js/core/checker.js
@@ -1,1 +1,28 @@
 // 检查数据解决方案
+
+function checkArray(array) {
+    const length = array.length;
+    const marks = new Array(length);
+    marks.fill(true);
+    for (let i = 0; i < length-1; i++) {
+        if (!marks[i]){
+            continue;
+        }
+        const v = array[i];
+        // 是否有效
+        if (!v) {
+            marks[i] = false;
+            continue;
+        }
+        // 是否有重复
+        for (let j = i + 1;j < length;j++) {
+            if (v === array[j]) {
+                marks[i] = marks[j] = false;
+            }
+        }
+    }
+    return marks;
+}
+/* 测试用代码 */
+// console.log(checkArray([2,3,3,4,5,6,7,8,9]));
+// console.log(checkArray([1,2,3,4,5,6,7,8,9]));
