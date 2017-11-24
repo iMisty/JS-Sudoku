b57f4f650fb8d20eab83216ad90f682fe4f7b2f4
JingYi <qianwumeng_2cyndi@qq.com>
2017-11-24 18:09:35 +0800

    数独生成解决方案

--- src/js/core/generator.js
+++ src/js/core/generator.js
@@ -1,37 +1,56 @@
 // 生成数独解决方案
-const Toolkit = require('./toolkit')
+const Toolkit = require('./toolkit');
 
 class Generator {
-  generate () {
-    this.matrix = Toolkit.matrix.makeMatrix()
-    // TODO 入口
-    for (let n = 1; n <= 9; n++) {
-      this.fillNumber(n)
-    }
+  generate(){
+      while (!this.interenalGenerate()) {
+        // TODO
+      }
   }
 
+  interenalGenerate () {
+    this.matrix = Toolkit.matrix.makeMatrix();
+    this.orders = Toolkit.matrix.makeMatrix()
+        .map(row => row.map((v, i) => i))
+        .map(row => Toolkit.matrix.shuffle(row));
+
+    for (let n = 1; n <= 9; n++) {
+      if (!this.fillNumber(n)){
+        return false;
+      }
+    }
+    return true;
+  }
+  /* Node 8.0.0 以上系统使用了npm 5.0，会引发内存溢出错误 
+   * 解决方法：
+   * 1.使用node 8以下的版本
+   * 2.单独将npm降级到3.xx版本
+   */
   fillNumber (n) {
-    this.fillNumber(n, 0)
+    return this.fillNumber(n, 0);
   }
 
   fillRow (n, rowIndex) {
     if (rowIndex > 8) {
-      return true
+      return true;
     }
-    const row = this.matrix[rowIndex]
+
+    const row = this.matrix[rowIndex];
+    const order = this.orders[rowIndex];
+
     for (let i = 0; i < 9; i++) {
-      const colIndex = i
+      const colIndex = orders[i];
       // 若有值，则跳过
       if (row[colIndex]) {
-        continue
+        continue;
       }
 
       // 检查是否允许填入n值
-      if (!Toolkit.matrix.checkFillable()) {
-        continue
+      if (!Toolkit.matrix.checkFillable(this.matrix, n, rowIndex, colIndex)) {
+        continue;
       }
 
-      row[colIndex] = n
+      row[colIndex] = n;
       // 当前行填写n成功则递归调用
       if (!this.fillRow(n, rowIndex + 1)) {
           row[colIndex] = 0;
@@ -40,7 +59,11 @@ class Generator {
 
       return true;
     }
-    
+
     return false;
   }
 }
+
+const generator = new Generator();
+generator.generate();
+console.log(generator.matrix);
--- www/js/index.js
+++ www/js/index.js
@@ -125,12 +125,50 @@
 	            [array[i], array[j]] = [array[j], array[i]];
 	        }
 	        return array;
+	    },
+	
+	    // TODO 检查是否允许填入n
+	    checkFillable(matrix, n, rowIndex, colIndex) {
+	        const row = matrix[rowIndex];
+	        const column = this.makeRow().map((v, i) => matrix[i][colIndex]);
+	        const { boxIndex } = boxToolkit.convertToBoxIndex(rowIndex, colIndex);
+	        const box = boxToolkit.getBoxCells(matrix, boxIndex);
+	        for (let i = 0; i < 9; i++) {
+	            if (row[i] === n || column[i] === n || box[i] === n) {
+	                return false;
+	            }
+	            return true;
+	        }
 	    }
 	};
 	
 	/* 宫坐标系工具 */
 	const boxToolkit = {
-	    // TODO
+	    getBoxCells(matrix, boxIndex) {
+	        const startRowIndex = Math.floor(boxIndex / 3) * 3;
+	        const startColIndex = boxIndex % 3 * 3;
+	        const result = [];
+	        for (let cellIndex = 0; cellIndex < 9; cellIndex++) {
+	            const rowIndex = startRowIndex + Math.floor(cellIndex / 3);
+	            const colIndex = startRowIndex + cellIndex % 3;
+	            result.push(matrix[rowIndex][colIndex]);
+	        }
+	        return result;
+	    },
+	
+	    convertToBoxIndex(rowIndex, colIndex) {
+	        return {
+	            boxIndex: Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3),
+	            cellIndex: rowIndex % 3 * 3 + colIndex % 3
+	        };
+	    },
+	
+	    convertFromBoxIndex(boxIndex, cellIndex) {
+	        return {
+	            rowIndex: Math.floor(boxIndex / 3) * 3 + Math.floor(cellIndex / 3),
+	            colIndex: boxIndex % 3 * 3 + cellIndex % 3
+	        };
+	    }
 	};
 	
 	module.exports = class Toolkit {
