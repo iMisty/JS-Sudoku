82016797bc6ee6fd988a72e53a42314b31c22227
JingYi <qianwumeng_2cyndi@qq.com>
2017-12-25 21:17:38 +0800

    TS

--- src/js/core/generator.js
+++ src/js/core/generator.js
@@ -1,70 +1,67 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // 生成数独解决方案
-const Toolkit = require('./toolkit');
-
-module.exports = class Generator {
-  generate(){
-      while (!this.interenalGenerate()) {
-        // TODO
-      }
-  }
-
-  interenalGenerate() {
-    this.matrix = Toolkit.matrix.makeMatrix();
-    this.orders = Toolkit.matrix.makeMatrix()
-        .map(row => row.map((v, i) => i))
-        .map(row => Toolkit.matrix.shuffle(row));
-
-    for (let n = 1; n <= 9; n++) {
-      if (!this.fillNumber(n)){
+var toolkit_1 = require("./toolkit");
+// const Toolkit = require('./toolkit');
+var Generator = /** @class */ (function () {
+    function Generator() {
+    }
+    Generator.prototype.generate = function () {
+        while (!this.interenalGenerate()) {
+            // TODO
+        }
+    };
+    Generator.prototype.interenalGenerate = function () {
+        this.matrix = toolkit_1.default.matrix.makeMatrix();
+        this.orders = toolkit_1.default.matrix.makeMatrix()
+            .map(function (row) { return row.map(function (v, i) { return i; }); })
+            .map(function (row) { return toolkit_1.default.matrix.shuffle(row); });
+        for (var n = 1; n <= 9; n++) {
+            if (!this.fillNumber(n)) {
+                return false;
+            }
+        }
+        return true;
+    };
+    /* Node 8.0.0 以上系统使用了npm 5.0，会引发内存溢出错误
+     * 解决方法：
+     * 1.使用node 8以下的版本
+     * 2.单独将npm降级到3.xx版本
+     * 3.使用尾递归 // TODO
+     */
+    Generator.prototype.fillNumber = function (n) {
+        return this.fillNumber(n, 0);
+    };
+    Generator.prototype.fillRow = function (n, rowIndex) {
+        if (rowIndex > 8) {
+            return true;
+        }
+        var row = this.matrix[rowIndex];
+        var order = this.orders[rowIndex];
+        for (var i = 0; i < 9; i++) {
+            var colIndex = orders[i];
+            // 若有值，则跳过
+            if (row[colIndex]) {
+                continue;
+            }
+            // 检查是否允许填入n值
+            if (!toolkit_1.default.matrix.checkFillable(this.matrix, n, rowIndex, colIndex)) {
+                continue;
+            }
+            row[colIndex] = n;
+            // 当前行填写n成功则递归调用
+            if (!this.fillRow(n, rowIndex + 1)) {
+                row[colIndex] = 0;
+                continue;
+            }
+            return true;
+        }
         return false;
-      }
-    }
-    return true;
-  }
-  /* Node 8.0.0 以上系统使用了npm 5.0，会引发内存溢出错误 
-   * 解决方法：
-   * 1.使用node 8以下的版本
-   * 2.单独将npm降级到3.xx版本
-   * 3.使用尾递归 // TODO
-   */
-  fillNumber(n) {
-    return this.fillNumber(n, 0);
-  }
-
-  fillRow(n, rowIndex) {
-    if (rowIndex > 8) {
-      return true;
-    }
-
-    const row = this.matrix[rowIndex];
-    const order = this.orders[rowIndex];
-
-    for (let i = 0; i < 9; i++) {
-      const colIndex = orders[i];
-      // 若有值，则跳过
-      if (row[colIndex]) {
-        continue;
-      }
-
-      // 检查是否允许填入n值
-      if (!Toolkit.matrix.checkFillable(this.matrix, n, rowIndex, colIndex)) {
-        continue;
-      }
-
-      row[colIndex] = n;
-      // 当前行填写n成功则递归调用
-      if (!this.fillRow(n, rowIndex + 1)) {
-          row[colIndex] = 0;
-          continue;
-      }
-
-      return true;
-    }
-
-    return false;
-  }
-}
-
+    };
+    return Generator;
+}());
+exports.Generator = Generator;
+exports.default = Generator;
 /* const generator = new Generator();
 generator.generate();
-console.log(generator.matrix); */
+console.log(generator.matrix); */ 
