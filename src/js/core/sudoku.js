d1109ed0ebf1018ea8f059595041db83269cbaec
JingYi <qianwumeng_2cyndi@qq.com>
2017-12-26 22:57:02 +0800

    TS

--- src/js/core/sudoku.js
+++ src/js/core/sudoku.js
@@ -1,24 +1,25 @@
+"use strict";
 // 生成游戏
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // 1.生成完成的解決方案
 // 2.随机去除部分数据
-
-const Generator  = require("./generator");
-
-module.exports = class sudoku {
-
-    constructor() {
+var generator_1 = require("./generator");
+var sudoku = /** @class */ (function () {
+    function sudoku() {
         // 完整的解决方案
-        const generator = new Generator();
+        var generator = new generator_1.default();
         generator.generate();
         this.solutionMatrix = generator.matrix;
     }
-
-    make(level = 5) {
+    sudoku.prototype.make = function (level) {
+        if (level === void 0) { level = 5; }
         // const shouldRid = Math.random() * 9 < level;
         // 生成谜盘
-        this.puzzleMatrix = this.solutionMatrix.map(row => {
-            return row.map(cell => Math.random() * 9 < level ? 0 : cell);
+        this.puzzleMatrix = this.solutionMatrix.map(function (row) {
+            return row.map(function (cell) { return Math.random() * 9 < level ? 0 : cell; });
         });
-    }
-}
+    };
+    return sudoku;
+}());
+exports.sudoku = sudoku;
+exports.default = Sudoku;
